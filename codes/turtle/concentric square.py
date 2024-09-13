0e17ce144bb6d45b578229a40367041cc2062b7e
Faizan Khan <faizankhan8960@gmail.com>
2024-09-13 10:54:02 +0530

    pyhon

---
+++ codes/turtle/concentric square.py
@@ -0,0 +1,7 @@
+from turtle import *
+speed ('fast')
+pencolor('red')
+for i in range(100):
+    fd(i*3+5)
+    lt(90)
+mainloop()
---
+++ codes/turtle/decagon.py
@@ -0,0 +1,7 @@
+from turtle import *
+
+t = Turtle()
+for i in range(10):
+    t.fd(100)
+    t.lt(36)
+mainloop()
